64d81b785a33fda869619f5609e0e50ab5bd7df0
Artemabramov004 <51322804+Artemabramov004@users.noreply.github.com>
2019-06-06 15:53:10 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,24 +1,16 @@
-var filed = document.getElementById("field");
-
-function isPrime(n) {
-    for(var i = 2; i < n; ++i) {
-        if(n % i == 0) {
-            return false;
+var button = document.getElementById("request");
+button.addEventListener("click", function() {
+    var xhr = new XMLHttpRequest();
+    xhr.open("get", "http:/localhost:591/", true);
+    xhr.onreadystatechange = function() {
+        if(this.readyState == this.DONE) {
+            if(this.status != 200) {
+                console.log("Ошибка: " + this.status);
+            } else {
+                console.log(this.responseText);
+            }
         }
-    }
+    };
 
-    return true;
-}
-
-filed.addEventListener("keydown", function(event) {
-    var number, i; 
-    if(event.which == 13) {
-        debugger;
-        number = Number(field.value);
-        alert(isPrime(number) ? "Простое!" : "Составное!");
-    }
-}); 
-
-
-
-
+    xhr.send();
+});
