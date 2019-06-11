e1d2c8bee45959ff43e0b7150e0f790497b7d296
Artemabramov004 <51322804+Artemabramov004@users.noreply.github.com>
2019-06-11 15:49:48 +0300

    11.06.2019
    Все что сделал

--- script.js
+++ script.js
@@ -1,16 +1,6 @@
-var button = document.getElementById("request");
-button.addEventListener("click", function() {
-    var xhr = new XMLHttpRequest();
-    xhr.open("get", "http:/localhost:591/", true);
-    xhr.onreadystatechange = function() {
-        if(this.readyState == this.DONE) {
-            if(this.status != 200) {
-                console.log("Ошибка: " + this.status);
-            } else {
-                console.log(this.responseText);
-            }
-        }
-    };
-
-    xhr.send();
+var vm = new Vue({
+    el: "#app",
+    data: {
+      query: "голый землекоп"
+    }
 });
